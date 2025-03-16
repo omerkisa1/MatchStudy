321a52a04b9adda9dd0b2ecec0c75ca053437d44
omerkisa1 <omereis.5555@gmail.com>
2025-03-16 13:31:38 +0300

    add user management endpoints for listing and deleting users

--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -1,12 +1,20 @@
 from fastapi import APIRouter
-from database.users import add_user, get_user_by_id
+from database.users import add_user, get_user_by_id, list_users,delete_user_by_id
 
 router = APIRouter()
 
 @router.post("/users")
-def create_user(email: str, password: str):
+async def add_user_endpoint(email: str, password: str):
     return add_user(email, password)
 
 @router.get("/users/{user_id}")
-def read_user(user_id: int):
+async def get_user_by_id_endpoint(user_id: int):
     return get_user_by_id(user_id)
+
+@router.get("/users")
+async def list_users_endpoint():
+    return list_users()
+
+@router.delete("/users/{user_id}")
+async def delete_user_by_id_enpoint(user_id: int):
+    return delete_user_by_id(user_id)
