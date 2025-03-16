907fb1689be0cae56cd0884b14d96b483565089c
omerkisa1 <omereis.5555@gmail.com>
2025-03-16 13:52:38 +0300

    user management endpoints with error handling and response messages

--- backend/routes/users_routes.py
+++ backend/routes/users_routes.py
@@ -1,20 +1,46 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from database.users import add_user, get_user_by_id, list_users,delete_user_by_id
 
 router = APIRouter()
 
 @router.post("/users")
 async def add_user_endpoint(email: str, password: str):
-    return add_user(email, password)
+    try:
+        add_user(email, password)
+        return {"message": "User added"}
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Kullanıcı eklenemedi.")
 
 @router.get("/users/{user_id}")
 async def get_user_by_id_endpoint(user_id: int):
-    return get_user_by_id(user_id)
+    try:
+        user = get_user_by_id(user_id)
+        if user:
+            return {"message": "User found", "user": user, "status": 200}
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Kullanıcı getirilemedi.")
 
 @router.get("/users")
 async def list_users_endpoint():
-    return list_users()
+    try:
+        users = list_users()
+        return {"message": "Users found", "users": users, "status": 200}
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Kullanıcılar getirilemedi.")
 
 @router.delete("/users/{user_id}")
 async def delete_user_by_id_enpoint(user_id: int):
-    return delete_user_by_id(user_id)
+    try:
+        delete_user_by_id(user_id)
+        return {"message": "User deleted"}
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Kullanıcı silinemedi.")
+
