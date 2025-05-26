56b6d16ac91c4ac602d5d3f3d9829011cfdac681
omerkisa1 <omereis.5555@gmail.com>
2025-05-26 08:19:53 +0300

    fix: Update CORS settings to allow production frontend URL and adjust Nginx proxy_pass configuration

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost", "http://localhost:80"],
+    allow_origins=["http://localhost:5173", "http://localhost", "http://localhost:80", "https://front-production-00d9.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
